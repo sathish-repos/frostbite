cb4e6717e40d6d893839cee5bb75816f6a46b89c
sathish <me.sathish.git@gmail.com>
2025-06-07 19:54:11 +0530

    fix: update header links and improve product rating display

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit  {
+export class HomeComponent implements OnInit {
+  environment = environment;
   ngOnInit() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
